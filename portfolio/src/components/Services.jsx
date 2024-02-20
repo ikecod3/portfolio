f2baee393ebb2deb6f01a9f1de77f1ad4c934fa4
IKE COD3 <ikechukwuokoye15@gmail.com>
2024-02-20 12:22:29 +0100

    adjusted button style on semi-md devices

--- portfolio/src/components/Services.jsx
+++ portfolio/src/components/Services.jsx
@@ -4,10 +4,10 @@ const Services = () => {
   return (
     <div
       id="services"
-      className="h-[40vh] flex flex-col  items-center justify-center p-8 mb-12 bg-neutral-50 bg-blend-difference"
+      className="h-[50vh]  flex flex-col  items-center justify-center p-8 mb-12 bg-gradient-to-l text-blue-600"
     >
-      <p className="text-center mx-auto max-w-screen-lg text-base sm:text-xl text-pretty px-4 sm:px-0">
-        <i className="tracking-wider ">
+      <p className="text-center mx-auto max-w-screen-lg text-base sm:text-xl text-pretty px-4 sm:px-0 ">
+        <i className="tracking-wider text-inherit">
           From dissecting data to weaving web wonders, my journey echoes
           versatility. <br /> As a full-stack web developer, I blend analytical
           prowess with a palette of technologies to craft scalable solutions
